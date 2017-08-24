c75cbb85bfab2a530a4e5b4fbd51c93617452a7b
chengwengao <657325704@qq.com>
2017-08-24 15:01:51 +0800

    首次提交

---
+++ superScoketSample/Program.cs
@@ -0,0 +1,128 @@
+﻿using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superScoketSample
+{
+    class Program
+    {
+        private static int port = 10002;   //监听端口
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start the server!");
+
+            Console.ReadKey();
+            Console.WriteLine();
+
+            var appServer = new AppServer();
+
+            //Setup the appServer
+            if (!appServer.Setup(port)) //Setup with listening port
+            {
+                Console.WriteLine("Failed to setup!");
+                Console.ReadKey();
+                return;
+            }else
+            {
+                Console.WriteLine("监听端口已启动："+port);
+                //处理连接，注册会话新建事件处理方法
+                appServer.NewSessionConnected += new SessionHandler<AppSession>(appServer_NewSessionConnected);
+                //注册请求处理方法
+                //appServer.NewRequestReceived += new RequestHandler<AppSession, StringRequestInfo>(appServer_NewRequestReceived);
+            }
+
+            Console.WriteLine();
+
+            //Try to start the appServer
+            if (!appServer.Start())
+            {
+                Console.WriteLine("Failed to start!");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("The server started successfully, press key 'q' to stop it!");
+
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                
+                Console.WriteLine();
+                continue;
+            }
+
+            //Stop the appServer
+            appServer.Stop();
+
+            Console.WriteLine("The server was stopped!");
+            Console.ReadKey();
+
+            
+
+        }
+
+        //在事件处理代码中发送欢迎信息给客户端
+        static void appServer_NewSessionConnected(AppSession session)
+        {
+            session.Send("Welcome to SuperSocket Telnet Server");
+        }
+
+        //实现请求处理
+        static void appServer_NewRequestReceived(AppSession session, StringRequestInfo requestInfo)
+        {
+            switch (requestInfo.Key.ToUpper())
+            {
+                case ("ECHO"):
+                    session.Send(requestInfo.Body);
+                    break;
+
+                case ("ADD"):
+                    session.Send(requestInfo.Parameters.Select(p => Convert.ToInt32(p)).Sum().ToString());
+                    break;
+
+                case ("MULT"):
+
+                    var result = 1;
+
+                    foreach (var factor in requestInfo.Parameters.Select(p => Convert.ToInt32(p)))
+                    {
+                        result *= factor;
+                    }
+
+                    session.Send(result.ToString());
+                    break;
+            }
+        }
+    }
+
+    //Command的用法,需要移除请求处理方法的注册，因为它和命令不能同时被支持
+    //Remove this line:appServer.NewRequestReceived += new RequestHandler<AppSession, StringRequestInfo>(appServer_NewRequestReceived);
+    public class ADD : CommandBase<AppSession, StringRequestInfo>
+    {
+        public override void ExecuteCommand(AppSession session, StringRequestInfo requestInfo)
+        {
+            session.Send(requestInfo.Parameters.Select(p => Convert.ToInt32(p)).Sum().ToString());
+        }
+    }
+
+    //Command的用法,需要移除请求处理方法的注册，因为它和命令不能同时被支持
+    //Remove this line:appServer.NewRequestReceived += new RequestHandler<AppSession, StringRequestInfo>(appServer_NewRequestReceived);
+    public class MULT : CommandBase<AppSession, StringRequestInfo>
+    {
+        public override void ExecuteCommand(AppSession session, StringRequestInfo requestInfo)
+        {
+            var result = 1;
+
+            foreach (var factor in requestInfo.Parameters.Select(p => Convert.ToInt32(p)))
+            {
+                result *= factor;
+            }
+
+            session.Send(result.ToString());
+        }
+    }
+}
